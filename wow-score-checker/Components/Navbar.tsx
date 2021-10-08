61904ad258849d2efaad04a35b31faa23be054c9
mikkelzu <mikkelzuuu@gmail.com>
2021-10-08 09:56:29 +0200

    initial search bar done

---
+++ wow-score-checker/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar nav-dark">
+            <a className="navbar-brand">Score Checker</a>
+        </nav>
+    )
+}
+
+export default Navbar
--- wow-score-checker/pages/_app.tsx
+++ wow-score-checker/pages/_app.tsx
@@ -1,7 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../Components/Navbar'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Navbar />
+        <Component {...pageProps} />
+      </>
+  )
 }
 export default MyApp
